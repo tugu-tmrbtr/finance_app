831639152172b1a907bf9d7e6801894cccc5bea9
Tuguldur <tugo.tmrbtr@gmail.com>
2021-11-20 20:20:00 +0800

    v.0.1.3

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-var uiController = (function () {
-  var x = 100;
-  function add(y) {
-    return x + y;
-  }
-  return {
-    publicAdd: function (a) {
-      b = add(a);
-      console.log("Боловсруулсан утга: " + b);
-    },
-  };
-})();
+// Delgetstei ajillah controller
+var uiController = (function () {})();
 
+// Sanhuute ajillah controller
 var financeController = (function () {})();
 
+// App holbogc controller
 var appController = (function (uiController, financeController) {
-  uiController.publicAdd(150);
+  var ctrlAddItem = function () {
+    console.log("jhjh");
+    // 1. Oruulah ugugdliig delgetsees olj avah.
+    // 2. Olj avsan ugugdluude financeController luu damjuulj tend hadgalna.
+    // 3. Olj avsan ugugdluude web deere gargaj haruulna.
+    // 4. Tusuhiig tootsoolno.
+    // 5. Etsiin uldegdel,  Tootsoog delgetsend haruulna.
+  };
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(uiController, financeController);
